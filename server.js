263965a84a55b8eecdd925bb501904ee6d67cc8c
Gaston Palmers <gaston.palmers@gmail.com>
2022-06-30 20:03:30 +1000

    added template

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+const PORT = 8000
+
+// let db,
+//     dbConnectionString = process.env.DB_STRING,
+//     dbName = 'sample_mflix',
+//     collection
+
+// MongoClient.connect(dbConnectionString)
+//     .then(client => {
+//         console.log(`Connected to Database`)
+//         db = client.db(dbName)
+//         collection = db.collection('movies')
+//     })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (request, response) => {
+    response.send("welcome to the jungle")
+})
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
