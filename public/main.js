c44aef3925000580ece8afb6458635d6120b40b8
Gaston Palmers <gaston.palmers@gmail.com>
2022-07-02 14:07:34 +1000

    added index.ejs

---
+++ public/main.js
@@ -0,0 +1,27 @@
+// Global variables
+document.querySelector('#searchButton').addEventListener('click', searchForBird)
+
+function searchForBird() {
+    const bird = document.querySelector('#searchInput').value
+    console.log(bird)
+    fetch(`https://bird-api-aus.herokuapp.com/api/${bird}`)
+        .then(response => {
+            console.log(typeof response)
+            console.log(response)
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            document.querySelector('#birdName').textContent = data.birdName
+            document.querySelector('#image').setAttribute('src', data.image)
+            document.querySelector('#scifiName').textContent = data.scifiName
+            document.querySelector('#description').textContent = data.description
+
+            
+
+            document.querySelector('#searchInput').value  = ''
+
+
+        })
+        
+}
